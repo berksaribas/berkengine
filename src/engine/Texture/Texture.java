073179b5d6cd6c0a97745361cad4d58e938e0e73
Berk Emre Saribas <berksaribas@sabanciuniv.edu>
2017-12-13 17:07:50 +0300

    Makes textures repeat as default.

--- src/engine/Texture/Texture.java
+++ src/engine/Texture/Texture.java
@@ -28,6 +28,10 @@ public class Texture {
         int result = glGenTextures();
 
         glBindTexture(GL_TEXTURE_2D, result);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
