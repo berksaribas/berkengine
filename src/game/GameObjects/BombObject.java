233990fe376fbc2cd2b4d527120da12f6547fd9c
Berk Emre Saribas <berksaribas@sabanciuniv.edu>
2017-12-21 15:06:16 +0300

    Bomb object for throwing and exploding.

---
+++ src/game/GameObjects/BombObject.java
@@ -0,0 +1,27 @@
+package game.GameObjects;
+
+import engine.Model.TexturedModel;
+import engine.Object.GameObject;
+import engine.Object.PhysicsComponent;
+import org.joml.Vector3f;
+
+public class BombObject extends GameObject {
+    enum State{
+        SET, EXPLODED
+    }
+
+    State state;
+
+    public BombObject(GameObject parent, TexturedModel model, Vector3f position, Vector3f rotation, Vector3f scale) {
+        super(parent, model, position, rotation, scale);
+        components.put(PhysicsComponent.class, new PhysicsComponent(this));
+    }
+
+    public void throwBomb(Vector3f direction) {
+        state = State.EXPLODED;
+        Vector3f momentum = new Vector3f(direction);
+        momentum.div(100f, 40f, 100f);
+        getByComponent(PhysicsComponent.class).addMomentum(momentum);
+    }
+
+}
