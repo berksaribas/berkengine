abfb475bd2550dc0908e441812d879798f519fe5
Berk Emre Saribas <berksaribas@sabanciuniv.edu>
2017-12-21 15:08:05 +0300

    Optimizes and simplifies the GameScene class.

--- src/game/Scenes/GameScene.java
+++ src/game/Scenes/GameScene.java
@@ -1,7 +1,7 @@
 package game.Scenes;
 
-import engine.Model.TexturedModel;
 import engine.Scene.Lightning.Shadow;
+import game.Controllers.BombController;
 import game.GameObjects.CubeObject;
 import game.GameObjects.PlayerObject;
 import game.Models.CrateModel;
@@ -14,23 +14,20 @@ import game.Models.CubeModel;
 import game.Skyboxes.ClearSkybox;
 import org.joml.Vector3f;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class GameScene extends Scene {
     FollowerCamera camera;
 
-    CrateModel crateModel;
-    CubeModel cubeModel;
-    TerrainModel terrainModel;
-
     PlayerObject player;
-    GameObject scene, ground, cylinder;
+
+    GameObject scene;
+
     Light light;
+
     Shadow shadow;
+
     ClearSkybox clearSkybox;
 
-    HashMap<TexturedModel, ArrayList<GameObject>> objects;
+    BombController bombController;
 
     public GameScene(int width, int height) {
         super(width, height);
@@ -40,7 +37,6 @@ public class GameScene extends Scene {
     protected void initializeLights() {
         light = new Light(new Vector3f(-0.2f, 1.0f, -0.3f), new Vector3f(1, 1, 1), 0);
         shadow = new Shadow(new Vector3f(light.getPosition().x, light.getPosition().y, light.getPosition().z));
-
     }
 
     @Override
@@ -50,35 +46,30 @@ public class GameScene extends Scene {
 
     @Override
     protected void initializeModels() {
-        crateModel = new CrateModel();
-        cubeModel = new CubeModel();
-        terrainModel = new TerrainModel();
+        modelController.addModel(CrateModel.class, new CrateModel());
+        modelController.addModel(CubeModel.class, new CubeModel());
+        modelController.addModel(TerrainModel.class, new TerrainModel());
     }
 
     @Override
     protected void createObjects() {
-
-
         clearSkybox = new ClearSkybox();
 
-        objects = new HashMap<>();
-        objects.put(crateModel, new ArrayList<>());
-        objects.put(cubeModel, new ArrayList<>());
-        objects.put(terrainModel, new ArrayList<>());
-
         scene = new GameObject(null, null, new Vector3f(0), new Vector3f(0),new Vector3f(1));
-        player = new PlayerObject(scene, cubeModel, new Vector3f(1.2f, 5f, 2.6f), new Vector3f(0, 0, 0), new Vector3f(0.075f, 0.2f, 0.075f));
 
-        //ground = new GameObject(scene, terrainModel, new Vector3f(0, -0.1f, 0), new Vector3f(0, 0, 0), new Vector3f(10f, 0.1f, 10f));
-        //objects.get(terrainModel).add(ground);
+        player = new PlayerObject(scene, modelController.getByModel(CubeModel.class), new Vector3f(1.2f, 5f, 2.6f), new Vector3f(0, 0, 0), new Vector3f(0.075f, 0.2f, 0.075f));
 
-        objects.get(cubeModel).add(new CubeObject(scene, cubeModel, new Vector3f(1f + 0.2f, -0.7f, 2.6f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f)));
+        objectController.addObject(new CubeObject(scene, modelController.getByModel(CubeModel.class), new Vector3f(1f + 0.2f, -0.7f, 2.6f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f)));
 
-        objects.get(cubeModel).add(new CubeObject(scene, cubeModel, new Vector3f(1f + 0.2f, 0.3f, 3.8f), new Vector3f(0, 0, 0), new Vector3f(0.2f, 0.2f, 0.2f)));
+        objectController.addObject(new CubeObject(scene, modelController.getByModel(CubeModel.class), new Vector3f(1f + 0.2f, 0.3f, 3.8f), new Vector3f(0, 0, 0), new Vector3f(0.2f, 0.2f, 0.2f)));
 
-        objects.get(cubeModel).add(new CubeObject(scene, cubeModel, new Vector3f(1f + 0.2f, -0.3f, 5f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f)));
+        objectController.addObject(new CubeObject(scene, modelController.getByModel(CubeModel.class), new Vector3f(1f + 0.2f, -0.3f, 5f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f)));
 
-        objects.get(cubeModel).add(new CubeObject(scene, cubeModel, new Vector3f(1f + 0.2f, 0.6f, 5f), new Vector3f(0, 0, 0), new Vector3f(0.4f, 0.4f, 0.4f)));
+        objectController.addObject(new CubeObject(scene, modelController.getByModel(CubeModel.class), new Vector3f(1f + 0.2f, 0.6f, 5f), new Vector3f(0, 0, 0), new Vector3f(0.4f, 0.4f, 0.4f)));
+
+        objectController.addObject(new CubeObject(scene, modelController.getByModel(CubeModel.class), new Vector3f(5f, 2f, 5f), new Vector3f(0, 0, 0), new Vector3f(1f, 1f, 1f)));
+
+        bombController = new BombController(objectController, modelController, scene, player);
 
     }
 
@@ -88,19 +79,29 @@ public class GameScene extends Scene {
 
         camera.handleFPSMovement(delta);
 
-        player.update(delta);
+        updateTree(scene, delta);
+
+        bombController.handle(camera, delta);
 
         camera.followObject();
 
         camera.update(objectShader);
 
         objectShader.setLight(light.getPosition(), light.getColor());
+
+    }
+
+    public void updateTree(GameObject parent, float delta) {
+        for(GameObject object : parent.getChildren()) {
+            object.update(delta);
+            updateTree(object, delta);
+        }
     }
 
     @Override
     protected void renderObjects() {
         objectShader.setDepthBias(shadow.getDepthBias());
-        renderer.renderObject(objects, objectShader, shadow);
+        renderer.renderObject(objectController.getObjects(), objectShader, shadow);
     }
 
     @Override
@@ -111,6 +112,6 @@ public class GameScene extends Scene {
 
     @Override
     protected void renderShadows() {
-        renderer.renderShadows(objects, shadow, shadowShader);
+        renderer.renderShadows(objectController.getObjects(), shadow, shadowShader);
     }
 }
