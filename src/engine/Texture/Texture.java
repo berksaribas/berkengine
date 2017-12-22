18c6a63a6232592592b69a0cd772cbc23cc7126e
Berk Emre Saribas <berksaribas@sabanciuniv.edu>
2017-12-22 19:26:59 +0300

    Implements creating empty textures and makes texture stuff more organized.

--- src/engine/Texture/Texture.java
+++ src/engine/Texture/Texture.java
@@ -8,56 +8,101 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.IntBuffer;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13.*;
 
 public class Texture {
 
     private int width, height;
-    private int texture;
+    private int id;
 
     public Texture(String path) {
-        texture = load(path);
-    }
-
-    private int load(String path) {
         ImageData imageData = TextureLoader.loadFile(path);
+
         width = imageData.getWidth();
         height = imageData.getHeight();
 
         int[] data = imageData.getData();
 
-        int result = glGenTextures();
+        generateTexture();
 
-        glBindTexture(GL_TEXTURE_2D, result);
+        bind();
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        setIntParam(GL_TEXTURE_WRAP_S, GL_REPEAT);
+        setIntParam(GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        setIntParam(GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        setIntParam(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+        enableAnisotrophicFiltering();
+
+        applyTexture(data, GL_RGBA, GL_UNSIGNED_BYTE);
+
+        unbind();
+
+    }
+
+    public Texture(int width, int height, int format, int type) {
+        this.width = width;
+        this.height = height;
+
+        generateTexture();
+
+        bind();
+
+        setIntParam(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        setIntParam(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        setIntParam(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+        setIntParam(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+        applyTexture(format, type);
+
+        unbind();
+    }
+
+    private void generateTexture() {
+        id = glGenTextures();
+    }
+
+    public void setIntParam(int pname, int param) {
+        glTexParameteri(GL_TEXTURE_2D, pname, param);
+    }
+
+    public void setFloatParam(int pname, int param, float[] arr) {
+        glTexParameterfv(pname, param, arr);
+    }
+
+    private void enableAnisotrophicFiltering() {
         float amount = GL11.glGetFloat(EXTTextureFilterAnisotropic.GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT);
         GL11.glTexParameterf(GL11.GL_TEXTURE_2D, EXTTextureFilterAnisotropic.GL_TEXTURE_MAX_ANISOTROPY_EXT, amount);
+    }
+
+    private void applyTexture(int[] data, int format, int type) {
 
         IntBuffer buffer = ByteBuffer.allocateDirect(data.length << 2)
                 .order(ByteOrder.nativeOrder()).asIntBuffer();
         buffer.put(data).flip();
 
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
-                GL_UNSIGNED_BYTE, buffer);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format,
+                type, buffer);
+
         GL30.glGenerateMipmap(GL_TEXTURE_2D);
-        glBindTexture(GL_TEXTURE_2D, 0);
-        return result;
+    }
+
+    private void applyTexture(int format, int type) {
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format,
+                type, 0);
     }
 
     public void bind() {
-        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindTexture(GL_TEXTURE_2D, id);
     }
 
     public void unbind() {
         glBindTexture(GL_TEXTURE_2D, 0);
     }
 
-    public int getTextureID() {
-        return texture;
+    public int getId() {
+        return id;
     }
 }
